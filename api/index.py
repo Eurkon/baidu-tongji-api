8c8dbfb31615d06ff66ec4672b67a442fdeb3bac
Eurkon <53698089+Eurkon@users.noreply.github.com>
2021-05-13 20:59:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,13 +8,21 @@ from datetime import datetime
 from urllib.parse import parse_qs
 
 
-def getdata(site_id, access_token):
+def get_data(site_id, access_token):
     end_date = datetime.now().strftime('%Y-%m-%d')
     year_ago = datetime.now() - relativedelta(years=1)
     start = datetime.now() - relativedelta(years=1, days=(5 - year_ago.weekday()))
     start_date = start.strftime('%Y-%m-%d')
-    url = r'https://openapi.baidu.com/rest/2.0/tongji/report/getData?access_token=' + access_token + '&site_id=' + site_id + '&start_date=' + start_date + '&end_date=' + end_date + '&metrics=pv_count&method=overview%2FgetTimeTrendRpt'
-    req = requests.get(url)
+    url = 'https://openapi.baidu.com/rest/2.0/tongji/report/getData?'
+    param = {
+        'access_token': access_token,
+        'site_id': site_id,
+        'start_date': start_date,
+        'end_date': end_date,
+        'metrics': 'pv_count',
+        'method ': 'overview/getTimeTrendRpt'
+    }
+    req = requests.post(url=url, data=param)
     return req.text
 
 
@@ -22,7 +30,9 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
         param = parse_qs(path.split('?')[1])
-        data = getdata(param['id'], param['token'])
+
+        data = get_data(param['id'], param['token'])
+        print(data)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
