f0d575cccd34c8cc0f2772b2adfb979fbc25c5fe
Eurkon <1124724692@qq.com>
2021-06-08 09:52:20 +0800

    fix: 修改为post

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+# @Author    : Eurkon
+# @Date      : 2021/6/5 10:19
 
 import json
 import requests
@@ -8,8 +10,16 @@ from http.server import BaseHTTPRequestHandler
 
 
 def get_data(params):
+    """重定向请求百度统计，解决跨域问题
+
+    Args:
+        params (dict): 参数字典，包括 site_id, access_token 等
+
+    Returns:
+        百度统计返回的 json
+    """
     url = 'https://openapi.baidu.com/rest/2.0/tongji/report/getData?'
-    req = requests.get(url=url, data=params)
+    req = requests.post(url=url, data=params)
     return req.json()
 
 
